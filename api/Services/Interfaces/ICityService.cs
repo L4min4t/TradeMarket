c63cf56e49f6376692213948f3d4b94c17757a4b
Ivan Vlasiuk <vlasiukk.i.v@gmail.com>
2024-01-21 22:59:08 +0200

    Basic services implemented

---
+++ api/Services/Interfaces/ICityService.cs
@@ -0,0 +1,8 @@
+﻿using Entities.Models.Application;
+
+namespace Services.Interfaces;
+
+public interface ICityService : IBaseService<City>
+{
+    
+}
---
+++ api/Services/Services/CityService.cs
@@ -0,0 +1,12 @@
+﻿using Entities.Models.Application;
+using Repositories.Interfaces;
+using Services.Interfaces;
+
+namespace Services.Services;
+
+public class CityService : BaseService<City>, ICityService
+{
+    public CityService(IBaseRepository<City> repository) : base(repository)
+    {
+    }
+}

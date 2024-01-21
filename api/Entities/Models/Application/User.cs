50d39380ee81b2e1c0f6ff01f64f4f9dc37417ae
Ivan Vlasiuk <vlasiukk.i.v@gmail.com>
2024-01-21 17:45:36 +0200

    entities created

---
+++ api/Entities/Models/Application/User.cs
@@ -0,0 +1,16 @@
+﻿namespace Entities.Models.Application;
+
+public class User
+{
+    public Guid UserId { get; set; } = Guid.NewGuid();
+
+    public string Name { get; set; } = null!;
+    public string? AvatarUrl { get; set; }
+    
+    public string? Email { get; set; }
+    public string? Phone { get; set; }
+    public string? Telegram { get; set; }
+    
+    public Guid? CityId { get; set; }
+    public City? City { get; set; }
+}

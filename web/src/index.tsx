43b6621a076c668abb65b6f6693ea03b7cb115f9
Ivan Vlasiuk <vlasiukk.i.v@gmail.com>
2024-03-27 19:04:22 +0200

    trying to deploy react app to github pages

--- web/src/index.tsx
+++ web/src/index.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import axios from "axios";
-import {BrowserRouter, Route, Routes} from "react-router-dom";
+import {BrowserRouter, HashRouter, Route, Routes} from "react-router-dom";
 
 import App from "./App";
 
